9e44531d13ffdc0b7b9149748a5d92d46433e789
Lorenzo <lorenzo1997chierisini@gmail.com>
2025-01-07 11:42:37 +0100

    Add mock  rooms data storage

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,14 @@
+import rooms from "@/data/rooms.json";
+
 export default function Home() {
   return (
     <>
-      <h1>Home Page</h1>
+      {rooms.length > 0 ? (
+        rooms.map((room) => <h3>{room.name}</h3>)
+      ) : ( 
+        <h3>No rooms available</h3>
+      )}  
+
     </>
   );
 }
