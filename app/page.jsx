61ec1751e0346561c4f1217df6774336c1441099
Lorenzo <lorenzo1997chierisini@gmail.com>
2025-01-07 13:45:40 +0100

    Add RoomCard component and update room display in Home page; refactor room data structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,18 @@
 import rooms from "@/data/rooms.json";
+import RoomCard from "@/components/RoomCard";
 
 export default function Home() {
   return (
     <>
       {rooms.length > 0 ? (
-        rooms.map((room) => <h3>{room.name}</h3>)
-      ) : ( 
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {rooms.map((room) => (
+            <RoomCard key={room.$id} room={room} />
+          ))}
+        </div>
+      ) : (
         <h3>No rooms available</h3>
-      )}  
-
+      )}
     </>
   );
 }
